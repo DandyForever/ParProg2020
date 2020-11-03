6a2156de3c5fff43294ca15dc3e4547c9b458608
DandyForever <32147889+DandyForever@users.noreply.github.com>
2020-11-03 20:41:46 +0300

    Done task Loop4
    All tests for task Loop4 passed

--- Loop4/src/main.cpp
+++ Loop4/src/main.cpp
@@ -8,14 +8,112 @@
 void calc(double* arr, uint32_t zSize, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
   if (rank == 0 && size > 0) {
-    for (uint32_t z = 1; z < zSize; z++) {
-      for (uint32_t y = 0; y < ySize - 1; y++) {
-        for (uint32_t x = 0; x < xSize - 1; x++) {
-          arr[z*ySize*xSize + y*xSize + x] = sin(arr[(z - 1)*ySize*xSize + (y + 1)*xSize + x + 1]);
-        }
-      }
-    }
-  }
+		double * tasks = (double*) malloc (xSize * ySize * zSize * sizeof(double));
+		uint32_t * task_size = (uint32_t *) malloc (xSize * ySize * zSize * sizeof(uint32_t));
+		uint32_t * flags = (uint32_t *) calloc (xSize * ySize * zSize, sizeof(uint32_t));
+		uint32_t current = 0, current_num = 0, index = 0, zCpy = 0;
+		int yCpy = 0, xCpy = 0;
+		for (uint32_t z = 0; z < zSize; z++) {
+			for (uint32_t y = 0; y < ySize; y++) {
+				for (uint32_t x = 0; x < xSize; x++) {
+					index = z * ySize * xSize + y * ySize + x;
+					if (!flags[index]) {
+						zCpy = z; yCpy = y; xCpy = x;
+						while (zCpy < zSize && yCpy >= 0 && xCpy >= 0) {
+							flags[index] = 1;
+							tasks[current++] = arr[index];
+							zCpy++; yCpy--; xCpy--;
+							index = zCpy * ySize * xSize + yCpy * ySize + xCpy;
+						}
+						task_size[current_num++] = current;
+					}
+				}
+			}
+		}
+
+		uint32_t block_size = (current + size) / size;
+		uint32_t current_proc = 0, current_index = 0, current_task = 0, last_guy = 0;
+		uint32_t task_number[10], task_weight[10];
+		for (uint32_t i = 0; i < current_num; i++) {
+			task_size[i] -= last_guy;
+			if (task_size[i] > block_size || i == current_num - 1) {
+				task_number[current_proc] = i - current_index;
+				task_weight[current_proc] = task_size[i];
+				current_index = i;
+				last_guy += task_size[i];
+				current_proc++;
+			}
+		}
+
+		for (int i = 1; i < size; i++) {
+			MPI_Send(&task_number[i], 1, MPI_UNSIGNED, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&task_weight[i], 1, MPI_UNSIGNED, i, 0, MPI_COMM_WORLD);
+		}
+
+		current_index = task_number[0], current_task = task_weight[0];
+		for (int i = 1; i < size; i++) {
+			MPI_Send(&tasks[current_task], task_weight[i], MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&task_size[current_index + 1], task_number[i], MPI_UNSIGNED, i, 0, MPI_COMM_WORLD);
+			current_task += task_weight[i];
+			current_index += task_number[i];
+		}
+
+		current_index = 0;
+		for (uint32_t i = 1; i < task_weight[0]; i++) {
+			if (i != task_size[current_index])
+				tasks[i] = sin(tasks[i - 1]);
+			else
+				current_index++;
+		}
+
+		current_index = task_number[0]; current_task = task_weight[0];
+		MPI_Status status;
+		for (int i = 1; i < size; i++) {
+			MPI_Recv(&tasks[current_task], task_weight[i], MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+			current_task += task_weight[i];
+		}
+
+		current = 0;
+		for (uint32_t z = 0; z < zSize; z++) {
+			for (uint32_t y = 0; y < ySize; y++) {
+				for (uint32_t x = 0; x < xSize; x++) {
+					index = z * ySize * xSize + y * ySize + x;
+					if (flags[index]) {
+						zCpy = z; yCpy = y; xCpy = x;
+						while (zCpy < zSize && yCpy >= 0 && xCpy >= 0) {
+							flags[index] = 0;
+							arr[index] = tasks[current++];
+							zCpy++; yCpy--; xCpy--;
+							index = zCpy * ySize * xSize + yCpy * ySize + xCpy;
+						}
+					}
+				}
+			}
+		}
+
+		free(tasks); free(task_size); free(flags);
+  } else {
+		uint32_t task_number = 0, task_weight = 0;
+		MPI_Status status;
+		MPI_Recv(&task_number, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&task_weight, 1, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, &status);
+		
+		double* local_task = (double*) malloc (task_weight * sizeof(double));
+		uint32_t* local_task_size = (uint32_t*) malloc (task_number * sizeof(uint32_t));
+		MPI_Recv(local_task, task_weight, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(local_task_size, task_number, MPI_UNSIGNED, 0, 0, MPI_COMM_WORLD, &status);
+
+		uint32_t current_index = 0;
+		for (uint32_t i = 1; i < task_weight; i++) {
+			if (i != local_task_size[current_index])
+				local_task[i] = sin(local_task[i - 1]);
+			else
+				current_index++;
+		}
+
+		MPI_Send(local_task, task_weight, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		free(local_task); free(local_task_size);
+	}
 }
 
 int main(int argc, char** argv)
