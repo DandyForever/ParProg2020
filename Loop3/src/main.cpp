bcf001a8018d3849e910dfd08684dc8bde5a2d48
DandyForever <32147889+DandyForever@users.noreply.github.com>
2020-11-03 20:41:06 +0300

    Done task Loop3
    All tests for task Loop3 passed

--- Loop3/src/main.cpp
+++ Loop3/src/main.cpp
@@ -5,19 +5,88 @@
 #include <unistd.h>
 #include <cmath>
 
+void calc_via_procs(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size);
+
 void calc(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
 {
+	double* arr_t;
+
   if (rank == 0 && size > 0)
   {
-    for (uint32_t y = 4; y < ySize; y++)
-    {
-      for (uint32_t x = 0; x < xSize; x++)
-      {
-        arr[y*xSize + x] = sin(arr[(y - 4)*xSize + x]);
-      }
-    }
+		arr_t = (double*) malloc (sizeof(double) * xSize * ySize);
+
+		for (uint32_t i = 0; i < ySize; i++)
+			for (uint32_t j = 0; j < xSize; j++)
+				arr_t[j * ySize + i] = arr[i * xSize + j];
   }
+
+	calc_via_procs(arr_t, xSize, ySize, rank, size);
+
+	if (rank == 0 && size > 0)
+	{
+		for (uint32_t i = 0; i < ySize; i++)
+			for (uint32_t j = 0; j < xSize; j++)
+				arr[i * xSize + j] = arr_t[j * ySize + i];
+
+		free(arr_t);
+	}
 }
+
+void calc_via_procs(double* arr, uint32_t ySize, uint32_t xSize, int rank, int size)
+{
+	MPI_Bcast(&xSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&ySize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	uint32_t block_size = 0, start = 0, end = 0;
+	if (rank == 0 && size > 0){
+		block_size = (ySize + size) / size;
+		for (int i = 1; i < size; i++) {
+			start = i * block_size;
+			end = start + block_size;
+			if (end > ySize)
+				end = ySize;
+			if (start > ySize)
+				start = ySize;
+			MPI_Send (&(arr[start * xSize]), xSize * (end - start), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+		}
+		start = 0;
+		end = block_size < ySize ? block_size : ySize;
+	} else {
+		block_size = (ySize + size) / size;
+		start = rank * block_size;
+		end = start + block_size;
+		if (end > ySize)
+			end = ySize;
+		if (start > ySize)
+			start = ySize;
+		arr = (double*) malloc (sizeof(double) * xSize * (end - start));
+		MPI_Status status;
+		MPI_Recv(arr, xSize * (end - start), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+	}
+
+	uint32_t index = 0;
+	for (uint32_t i = 0; i < end - start; i++)
+		for (uint32_t j = 4; j < xSize; j++) {
+			index = i * xSize + j;
+			arr[index] =  sin(arr[index - 4]);
+		}
+	if (rank == 0 && size > 0) {
+		for (int i = 1; i < size; i++) {
+			start = i * block_size;
+			end = start + block_size;
+			if (end > ySize)
+				end = ySize;
+			if (start > ySize)
+				start = ySize;
+			MPI_Status status;
+			MPI_Recv (&(arr[start * xSize]), xSize * (end - start), MPI_DOUBLE, i, 0, MPI_COMM_WORLD, &status);
+		}
+	} else {
+		MPI_Send(arr, xSize * (end - start), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		free(arr);
+	}
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -93,7 +162,7 @@ int main(int argc, char** argv)
       output << std::endl;
     }
     output.close();
-    delete arr;
+    delete [] arr;
   }
 
   MPI_Finalize();
