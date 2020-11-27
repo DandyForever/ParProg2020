722230b295bcf68cbd666cd2ac8bb87a1e109a76
DandyForever <dandy4ever31@gmail.com>
2020-11-27 15:47:21 +0300

    Done CompMath1

--- CompMath1/src/main.cpp
+++ CompMath1/src/main.cpp
@@ -12,6 +12,131 @@ double acceleration(double t)
 
 void calc(double* trace, uint32_t traceSize, double t0, double dt, double y0, double y1, int rank, int size)
 {
+    MPI_Bcast(&traceSize, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&t0, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    uint32_t block_size = (traceSize + size) / size;
+    uint32_t start = rank * block_size;
+    uint32_t finish = rank * block_size + block_size;
+    finish = (finish > traceSize) ? traceSize : finish;
+
+    if (rank != 0) {
+        trace = new double[finish - start];
+    }
+
+    trace[0] = 0.;
+    trace[1] = 0.;
+    if (rank == 0) {
+        trace[0] = y0;
+        trace[1] = y0;
+    }
+    for (uint32_t i = start + 2; i < finish; i++) {
+        uint32_t index = i - start;
+        trace[index] = dt * dt * acceleration(t0 + (i - 1) * dt) + 2 * trace[index - 1] - trace[index - 2];
+    }
+    
+    uint32_t calc_size = finish - start;
+    double* calculated;
+    int* blocks_size, *shifts;
+
+    if (rank == 0) {
+        blocks_size = new int[size];
+        shifts = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            start = i * block_size;
+            finish = i * block_size + block_size;
+            finish = (finish > traceSize) ? traceSize : finish;
+            blocks_size[i] = finish - start;
+            shifts[i] = start;
+        }
+        
+        calculated = new double[traceSize];
+
+        MPI_Gatherv(trace, calc_size, MPI_DOUBLE, calculated, blocks_size, shifts, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        
+
+        double b = 0.;
+        double u = 0.;
+        double v = 0.;
+        double a = y0;
+        double a_add = 0.;
+        double v_add = 0.;
+        uint32_t ft = 0, lt = 0;
+        for (int i = 1; i < size; i++) {
+            u = (trace[shifts[i] - 1] - trace[shifts[i] - 2]);
+            b = trace[shifts[i] - 1];
+            a_add = a + b + v * (blocks_size[i - 1]);
+            v_add = u + v;
+            ft = shifts[i];
+            if (i == size - 1)
+                lt = traceSize;
+            else
+                lt = shifts[i + 1];
+
+            for (uint32_t j = ft; j < lt; j++) {
+                uint32_t param = j - ft;
+                trace[j] = calculated[j] + a_add + param * v_add;
+            }
+
+            a = trace[lt - 1];
+            v = u;
+        }
+
+        u = (y1 - trace[traceSize - 1]) / (traceSize);
+        a = 0.;
+        b = 0.;
+        v = 0.;
+        a_add = 0.;
+        v_add = u;
+
+        for (int i = 0; i < size; i++) {
+            ft = shifts[i];
+            if (i == size - 1)
+                lt = traceSize;
+            else
+                lt = shifts[i + 1];
+
+            for (uint32_t j = ft; j < lt; j++) {
+                uint32_t param = j - ft;
+                trace[j] = calculated[j] + a_add + param * v_add;
+            }
+
+            a = trace[ft];
+            v = u;
+            u = (trace[lt - 1] - trace[lt - 2]);
+            b = trace[lt - 1];
+            a_add = a + b + v * (blocks_size[i]);
+            v_add = u + v;
+        }
+
+        delete [] calculated;
+        delete [] shifts;
+        delete [] blocks_size;
+
+    } else {
+        MPI_Gatherv(trace, calc_size, MPI_DOUBLE, calculated, blocks_size, shifts, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        delete [] trace;
+    }
+/*
+        double v = 0.;
+        double a = y0;
+        double u = trace[calc_size - 1] / (calc_size * dt);
+        double b = trace[calc_size - 1];
+        
+        MPI_Send(&u, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&v, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&a, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        MPI_Send(&b, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+
+        double b_ = 0.;
+        MPI_Status status;
+        MPI_Recv(&b_, 1, MPI_DOUBLE, size - 1, 0, MPI_COMM_WORLD, &status);
+        v0 = (y1 - b_) / (dt * traceSize);
+
+
   // Sighting shot
   double v0 = 0;
   if (rank == 0 && size > 0)
@@ -35,7 +160,7 @@ void calc(double* trace, uint32_t traceSize, double t0, double dt, double y0, do
       trace[i] = dt*dt*acceleration(t0 + (i - 1)*dt) + 2*trace[i - 1] - trace[i - 2];
     }
   }
-
+*/
 }
 
 int main(int argc, char** argv)
